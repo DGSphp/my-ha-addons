69568fbbed838f947c8767d7a7069dd74ffc87d5
DGSphp <131772724+DGSphp@users.noreply.github.com>
2025-12-14 22:38:45 +0700

    Delete bluetooth-manager directory

--- bluetooth-manager/types.ts
+++
@@ -1,8 +0,0 @@
-
-export interface Device {
-  id: string;
-  name: string | null;
-  device: BluetoothDevice;
-}
-
-export type ConnectionStatus = 'disconnected' | 'connecting' | 'connected' | 'error';
